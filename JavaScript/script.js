bdc9bcabefdbe868055fdb6b94ac52bc44a26e14
Ricardo Hemmel <ricardo.hemmel@hotmail.com>
2023-06-11 15:42:59 -0300

    .

--- JavaScript/script.js
+++
@@ -1,6 +0,0 @@
-let sideMenu = document.querySelector(".hamburger-menu");
-
-sideMenu.addEventListener("click", function(){
-    document.querySelector(".side-bar").classList.toggle("show-menu")
-    document.querySelector(".hamburger-menu").classList.toggle("hamburger-transition")
-})
